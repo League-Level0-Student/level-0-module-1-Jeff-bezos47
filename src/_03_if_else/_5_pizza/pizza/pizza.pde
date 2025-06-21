5e8a6715c4e541a0983b77c5e64fd2bf386a2c9e
league <bobby>
2025-06-21 11:29:37 -0700

    dlkjsld;kj

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,19 @@
-
+PImage crayon;
 void setup() {
+    size(500,500);
+    fill(#E8B562);
+    ellipse(250, 250, 450, 450);
     
+     crayon = loadImage("bluecrayon(1).jpeg");
+     crayon.resize(60, 60);
 }
 void draw() {
-    
+ image(crayon, 150, 200); 
+   image(crayon,20, 200);
+   image(crayon,200, 100);
+   image(crayon,36, 47);
+   image(crayon, 395, 400);
+   image(crayon, 295, 300);
+   image(crayon, 130, 345);
+  image(crayon, 290, 100);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,25 +1,49 @@
 package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
+
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String input = JOptionPane.showInputDialog("What starts with T, ends with T, and has T in it?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(input.equals("A teapot")) {
+			JOptionPane.showMessageDialog(null,"Corrtect!");
+			score+=1;
+		}
 
+
+		else {
+			JOptionPane.showMessageDialog(null,"WRONG!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+		String imput = JOptionPane.showInputDialog("What has a head a tail, but no body");
+		if(imput.equals("a coin")) {
+			JOptionPane.showMessageDialog(null,"Corrtect!");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"WRONG!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
+		String inmput = JOptionPane.showInputDialog("what goes up but never comes down");
+		if(inmput.equals("your age")){
+			JOptionPane.showMessageDialog(null,"Corrtect!");
+			score+=1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"WRONG!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
+			// 2. Make a pop up to show the score.
+		String imnput = JOptionPane.showInputDialog("Your final score was" + score+ ".");
 		
 	}
 }
-
