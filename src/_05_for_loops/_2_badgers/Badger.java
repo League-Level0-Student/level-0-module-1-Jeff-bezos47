024a930b172beea2a13abdccdf2c3646f6ff521d
league <h>
2025-07-26 11:31:14 -0700

    dcfvgbnm

--- src/_05_for_loops/_2_badgers/Badger.java
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -1,5 +1,30 @@
 package _05_for_loops._2_badgers;
 
 public class Badger {
-
+public static void main(String[] args) {
+	
+		
+	for(int twice=0;twice<2;twice++) {
+		for (int badger=0; badger<12; badger++) {
+			System.out.print("badger");
+			if(badger < 11) {
+			System.out.print(", ");
+			}
+		}
+		System.out.println();
+		
+		for(int mushroom=0; mushroom<2; mushroom++) {
+		System.out.print("mushroom");
+		if(mushroom < 1) {
+		System.out.print(", ");
+		}
+		}
+		System.out.println();
+		System.out.println();
+	}
+	System.out.println();
+	
+	System.out.println("A snake!!!");
+	
+}
 }
